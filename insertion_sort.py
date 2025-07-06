2119016c2326624596e0728af1fd12d3bfd727be
Sudhanshu Dash <sdash33968@ucumberlands.edu>
2025-07-06 18:56:02 -0500

    Commit 3: Added test case and output

--- insertion_sort.py
+++ insertion_sort.py
@@ -1,9 +1,5 @@
-def insertion_sort_desc(arr):
-    for i in range(1, len(arr)):
-        key = arr[i]
-        j = i - 1
-        while j >= 0 and arr[j] < key:
-            arr[j + 1] = arr[j]
-            j -= 1
-        arr[j + 1] = key
-    return arr
+if __name__ == "__main__":
+    sample_list = [9, 2, 14, 7, 3]
+    print("Original list:", sample_list)
+    sorted_list = insertion_sort_desc(sample_list)
+    print("Sorted list (descending):", sorted_list)
